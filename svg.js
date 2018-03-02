edf020a9af1c66c98ed2ef672f42a766126f4d64
kchen36 <kchen36@stuy.edu>
2018-03-01 19:58:16 -0500

    done

--- svg.js
+++ svg.js
@@ -1,16 +1,18 @@
-var r = 1;
+var r = 25, x = 250, y = 250;
 increase = true;
+var xplus = true;
+var yplus = true;
 frame = null;
 var c = null;
 var pic = document.getElementById("svg");
 var btn = document.getElementById("clear");
 var grow = document.getElementById('grow');
-var bounce = document.getElementById("bounce");
+var bounceb = document.getElementById("bounce");
 
 var circle = function(e){
     c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx","250");
-    c.setAttribute("cy", "250");
+    c.setAttribute("cx", x + "");
+    c.setAttribute("cy", y + "");
     c.setAttribute("r", r + "");
     c.setAttribute("fill", "black");
     c.setAttribute("stroke","black");
@@ -42,13 +44,51 @@ var draw = function(e){
     frame = setInterval(resize, 10);
 }
 
+var bounce = function(){
+    if (pic.hasChildNodes()){
+	clear();
+    }
+    circle();
+    frame = setInterval(bouncehelper, 10);
+}
+var bouncehelper= function(){
+    if (x <= 25){
+	xplus = true;
+    }
+    if (x >= 475){
+	xplus = false;
+    }
+    if (y <= 25){
+	yplus = true;
+    }
+    if (y >= 476){
+	yplus = false;
+    }
+    if (xplus){
+	x += 5;
+    }
+    else{
+	x -= 5;
+    }
+    if (yplus){
+	y += 5;
+    }
+    else{
+	y -= 5;
+    }
+    c.setAttribute("cx", x + "");
+    c.setAttribute("cy", y + "");
+}
+
 var clear = function() {
-    r = 1;
+    x = 250;
+    y = 250;
+    r = 25;
     increase = true;
     pic.removeChild(pic.firstChild);
     clearInterval(frame)
 }
 
 grow.addEventListener("click",draw);
-bounce.addEventListener("click",bounce);
+bounceb.addEventListener("click",bounce);
 btn.addEventListener("click",clear);
